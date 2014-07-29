00a958c8e0eba337094053edcd247d095b2a6963
John Wright <jcwright@mit.edu>
2014-07-29 02:11:29 +0000

    code formatting.

--- mpo.py
+++ mpo.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
  #    sys.path.insert(0, cmd_folder)
 
  # use this if you want to include modules from a subforder
-    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"client/python")))
+    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
+                    inspect.getfile( inspect.currentframe() ))[0],"client/python")))
     if cmd_subfolder not in sys.path:
         sys.path.insert(0, cmd_subfolder)
 
