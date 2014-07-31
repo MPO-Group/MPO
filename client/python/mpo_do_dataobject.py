f7776260e9888938473c9fb3f80701c71deb68c1
Josh Stillerman <jas@psfc.mit.edu>
2014-07-31 21:56:43 +0000

    Beginnings of an rsync data object. Add generic arguments to super class for workflow and parent
    The parser method cli will eventually have to be broken out into: 
      cli_create
      cli_ls
      cli_restore
    methods.  When this happens many of the create generic args will have to move
    to a new method in the super class, so that ls and restore can potentially operate 
    without them (to be designed)
    
    NOTE: this change to mpo_do_dataobject BREAKS 
       mpo create -p filesys
       mpo create -p mdsplus

--- client/python/mpo_do_dataobject.py
+++ client/python/mpo_do_dataobject.py
@@ -15,5 +15,14 @@ class mpo_do_dataobject(object):
                                          prog='mpo create --protocol=xxx')
         self.parser.add_argument('--verbose','-v',action='store_true',help='Turn on debugging info',
                             default=False)
-        self.parser.add_argument('--description', '--desc', '-d', action='store', help='''An optional description for this data object''')
-        self.parser.add_argument('--name', '-n', action='store', help='''An optional name for this data object''', required=True)
+        self.parser.add_argument('--description', '--desc', '-d', action='store', 
+                                 help='''An optional description for this data object''')
+        self.parser.add_argument('--name', '-n', action='store', 
+                                 help='''An optional name for this data object''', required=True)
+        group = self.parser.add_mutually_exclusive_group(required=False)
+        group.add_argument('--workflow_id','--wid', '-w', action='store', 
+                           help='the guid of the workflow this data object is part of')
+        group.add_argument('--composite_id','--cid', '-c', action='store', 
+                           help='the composite id of the workflow this data object is part of')
+        self.parser.add_argument('--parent_uid', '--parent', '-P', action='store', 
+                                 help='The uuid of the parent of this data object')
