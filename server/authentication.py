12e9c67b7193b84cc71b66d35de283766eea6a53
Liz Coviello <covielloe@fusion.gat.com>
2015-05-29 21:58:18 +0000

    Minor change to handle different DN formats for MPO-UI-SERVER.crt.  Instead of looking for the exact match '/C=US/ST=California/L=LaJolla/O=General Atomics/O=c21f969b5f03d33d43e04f8f136e7682/OU=GAT/CN=MPO-UI-SERVER/emailAddress=abla@fusion.gat.com' in the DN, it now looks for '*abla@fusion.gat.com*'.

--- server/authentication.py
+++ server/authentication.py
@@ -6,13 +6,10 @@ def get_user_dn(request):
                 try:
                         ans =  request.environ['SSL_CLIENT_S_DN']
                 except:
-                        try:
-                                ans = request.environ['DEMO_AUTH']
-                        except:
-                                ans = ''
+                        ans = ''
 
-        #If DN is that of the UI, grab our special header for the user DN
-	if ans == '/C=US/ST=California/L=LaJolla/O=General Atomics/O=c21f969b5f03d33d43e04f8f136e7682/OU=GAT/CN=MPO-UI-SERVER/emailAddress=abla@fusion.gat.com' :
+        #If DN is that of the UI cert to API (MPO-UI-SERVER.crt), grab our special header for the user DN
+        if ans.find('abla@fusion.gat.com')>-1:
 		ans = request.headers['Real-User-DN'] 
 #        if len(ans) == 0:
 #                ans = getpass.getuser()
