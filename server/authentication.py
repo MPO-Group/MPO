951375568edacc14771f5c464e7fc9edab857d5d
Josh Stillerman <jas@psfc.mit.edu>
2015-06-10 14:03:20 +0000

    parse the server certificate and the webserver provided certificate, and then compare the results, instead of just comparing the strings

--- server/authentication.py
+++ server/authentication.py
@@ -1,3 +1,13 @@
+import re
+ 
+dn_re = re.compile(r'(?:/)(.+?)(?:=)([^/]+)')
+ 
+def parse_dn(dn):
+  return dict(dn_re.findall(dn))
+
+server_dn = '/C=US/ST=California/L=LaJolla/O=General Atomics/O=c21f969b5f03d33d43e04f8f136e7682/OU=GAT/CN=MPO-UI-SERVER/emailAddress=abla@fusion.gat.com'
+server_dict = parse_dn(server_dn)
+
 def get_user_dn(request):
 #        import getpass
         try:
@@ -12,7 +22,8 @@ def get_user_dn(request):
                                 ans = ''
 
         #If DN is that of the UI cert to API (MPO-UI-SERVER.crt), grab our special header for the user DN
-        if ans.find('abla@fusion.gat.com')>-1:
+	dn_dict = parse_dn(ans)
+        if cmp(dn_dict, server_dict) == 0:
 		ans = request.headers['Real-User-DN'] 
 #        if len(ans) == 0:
 #                ans = getpass.getuser()
