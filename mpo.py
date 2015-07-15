a512e2e113e13b7d2d423100ddb126825472ca8e
John Wright <jcwright@mit.edu>
2015-07-15 17:02:40 +0000

    Minor updates to mpo.py wrapper to look for system mporc file.

--- mpo.py
+++ mpo.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python2.7
 from __future__ import print_function
 if __name__ == '__main__':
     import os, sys, inspect
- # realpath() with make your script run, even if you symlink it :)
- #cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
- #if cmd_folder not in sys.path:
- #    sys.path.insert(0, cmd_folder)
+    from ConfigParser import SafeConfigParser
 
- # use this if you want to include modules from a subfolder
+    DEFAULT_API_SERVER = 'https://mpo.psfc.mit.edu/test-api'
+    DEFAULT_API_VERSION = 'v0'
+    DEFAULT_API_AUTH = '/etc/MPO Demo User.pem'
+
     #install path to mpo_arg
     cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
                     inspect.getfile( inspect.currentframe() ))[0],"./client/python")))
@@ -22,25 +22,50 @@ if __name__ == '__main__':
 
     from mpo_arg import mpo_cli
 
-    #default preferences. overridden by environment
-    mpo_version    = os.getenv('MPO_VERSION','v0')
-    mpo_api_url    = os.getenv('MPO_HOST', 'https://localhost/api') #API_URL
-    mpo_cert       = os.getenv('MPO_AUTH', '~/.mpo/mpo_cert')
-    archive_host   = os.getenv('MPO_ARCHIVE_HOST', 'psfcstor1.psfc.mit.edu')
-    archive_user   = os.getenv('MPO_ARCHIVE_USER', 'psfcmpo')
-    archive_key    = os.getenv('MPO_ARCHIVE_KEY', '~/.mporsync/id_rsa')
-    archive_prefix = os.getenv('MPO_ARCHIVE_PREFIX', 'mpo-persistent-store/')
-
 #load user prefs here, these override any environmental settings or the above defaults
+
+#first load any global configuration
+    globalconf = '/etc/mporc'
+    if os.path.isfile(globalconf):
+        sparser = SafeConfigParser()
+        sparser.read(globalconf)
+        mpo_api_url = sparser.get('api','MPO_HOST')
+        mpo_version = sparser.get('api','MPO_VERSION')
+        mpo_cert = sparser.get('api','MPO_AUTH')
+    else: #defaults if no global settings
+        mpo_api_url = DEFAULT_API_SERVER
+        mpo_version = DEFAULT_API_VERSION
+        mpo_cert    = DEFAULT_API_AUTH
+
+#user overrides, either in .mpo directory or .mporc file
     userconfdir = os.getenv('HOME')+'/.mpo/'
     userconf = userconfdir+'mpo.conf'
+    uconf = False
     if os.path.isfile(userconf):
-        from ConfigParser import SafeConfigParser
-        parser = SafeConfigParser()
-        parser.read(userconfdir+'/mpo.conf')
-        mpo_api_url = parser.get('api','MPO_HOST')
-        mpo_cert = userconfdir+parser.get('api','MPO_AUTH')
+        uconf=userconf
+    elif os.path.isfile(os.getenv('HOME')+'/.mporc'):
+        uconf=os.getenv('HOME')+'/.mporc'
 
+    if uconf:
+        parser = SafeConfigParser()
+        parser.read(uconf)
+        if parser.has_option('api','MPO_HOST'):
+            mpo_api_url = parser.get('api','MPO_HOST')
+        if parser.has_option('api','MPO_AUTH'):
+            mpo_cert = parser.get('api','MPO_AUTH')
+
+#environmental overrides last
+    if os.environ.has_key('MPO_VERSION'):
+        mpo_version    = os.getenv('MPO_VERSION')
+    if os.environ.has_key('MPO_HOST'):
+        mpo_api_url    = os.getenv('MPO_HOST')
+    if os.environ.has_key('MPO_AUTH'):
+        mpo_cert       = os.getenv('MPO_AUTH')
+
+#    archive_host   = os.getenv('MPO_ARCHIVE_HOST', 'psfcstor1.psfc.mit.edu')
+#    archive_user   = os.getenv('MPO_ARCHIVE_USER', 'psfcmpo')
+#    archive_key    = os.getenv('MPO_ARCHIVE_KEY', '~/.mpo/id_rsa')
+#    archive_prefix = os.getenv('MPO_ARCHIVE_PREFIX', 'mpo-persistent-store/')
    
     cli_app=mpo_cli(version=mpo_version, api_url=mpo_api_url, 
                     mpo_cert=mpo_cert)    
