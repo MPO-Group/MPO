0d91b3f9eb4406b189e0ceac793120738b960c3e
Alex Romosan <aromosan@lbl.gov>
2015-07-22 17:27:47 +0000

    fix typos.

--- db/add_mpoauth.sql
+++ db/add_mpoauth.sql
@@ -5,8 +5,8 @@ create table mpoauth
   read boolean,
   write boolean
 );
-inert into mpoauth (u_guid, read, write) values ('bc789de3-7484-49dc-a498-3b5a3aad3c80', true, true)
-inert into mpoauth (u_guid, read, write) values ('f223db41-d1c5-41db-b8af-fde6c0a16f76', true, true)
+insert into mpoauth (u_guid, read, write) values ('bc789de3-7484-49dc-a498-3b5a3aad3c80', true, true);
+insert into mpoauth (u_guid, read, write) values ('f223db41-d1c5-41db-b8af-fde6c0a16f76', true, true);
 alter table mpoauth OWNER TO mpoadmin;
 alter table mpoauth add constraint mpoauth_u_guid_fkey FOREIGN KEY (u_guid) REFERENCES mpousers(uuid);
 
