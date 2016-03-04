341aa8d48d0e1745af923e72103b9128d7e9cef7
Alex Romosan <aromosan@lbl.gov>
2016-03-04 08:44:46 -0800

    now you can really mix specifying ontology terms by either uid or path together.

--- db/db.py
+++ db/db.py
@@ -313,6 +313,10 @@ def getOntologyTermCount(id='0',queryargs={},dn=None):
     Returns the count of ontology terms by instance values.
     """
 
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = mypool.connect()
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor(cursor_factory=psyext.RealDictCursor)
     #Construct query for database
     q = 'SELECT a.ot_guid AS uid, a.parent_guid AS parent_uid, a.name AS name, c.value, count(*) FROM ontology_terms a, mpousers b, ontology_instances c, workflow d where c.term_guid=a.ot_guid and c.target_guid=d.w_guid and d.u_guid=b.uuid'
 
@@ -335,32 +339,22 @@ def getOntologyTermCount(id='0',queryargs={},dn=None):
         q+=' and b.firstname ilike \'%%'+queryargs['firstname']+'%%\''
     if queryargs.has_key('term'):
         q+=' and target_guid in ('
-        du = defaultdict(list)
-        dp = defaultdict(list)
+        d = defaultdict(list)
         for key in json.loads(queryargs['term']):
             if key.has_key('uid'):
-                du[key['uid']].append(key['value'])
+                d[key['uid']].append(key['value'])
             elif key.has_key('path'):
-                dp[key['path']].append(key['value'])
-        for k,l in du.iteritems():
+                cursor.execute('select getTermUidByPath(%s) as uid',(key['path'],))
+                d[cursor.fetchone()['uid']].append(key['value'])
+        for k,l in d.iteritems():
             s = 'select target_guid from ontology_terms a, ontology_instances c where c.term_guid=a.ot_guid and ('
             for m in l:
                 s+= '('+query_map['ontology_terms']['uid']+'=%s'+' and '+query_map['ontology_instances']['value']+'=%s) or '
                 v+=(k,m)
             q+=s[:-4]+') intersect '
-        for k,l in dp.iteritems():
-            s = 'select target_guid from ontology_terms a, ontology_instances c where c.term_guid=a.ot_guid and ('
-            for m in l:
-                s+= '('+query_map['ontology_terms']['uid']+'=getTermUidByPath(%s)'+' and '+query_map['ontology_instances']['value']+'=%s) or '
-                v+=(k,m)
-            q+=s[:-4]+') intersect '
         q=q[:-11]+')'
 
     q+=' group by uid, parent_uid, a.name, c.term_guid,value order by a.name,c.value'
-    # get a connection, if a connect cannot be made an exception will be raised here
-    conn = mypool.connect()
-    # conn.cursor will return a cursor object, you can use this cursor to perform queries
-    cursor = conn.cursor(cursor_factory=psyext.RealDictCursor)
     # execute our Query
     cursor.execute(q,v)
     # retrieve the records from the database
@@ -683,23 +677,17 @@ def getWorkflow(queryargs={},dn=None):
 
     if queryargs.has_key('term'):
         q+=' and w_guid in ('
-        du = defaultdict(list)
-        dp = defaultdict(list)
+        d = defaultdict(list)
         for key in json.loads(queryargs['term']):
             if key.has_key('uid'):
-                du[key['uid']].append(key['value'])
+                d[key['uid']].append(key['value'])
             elif key.has_key('path'):
-                dp[key['path']].append(key['value'])
-        for k,l in du.iteritems():
+                cursor.execute('select getTermUidByPath(%s) as uid',(key['path'],))
+                d[cursor.fetchone()['uid']].append(key['value'])
+        for k,l in d.iteritems():
             s = 'select target_guid from ontology_terms a, ontology_instances c where c.term_guid=a.ot_guid and ('
             for m in l:
                 s+= '('+query_map['ontology_terms']['uid']+'=%s'+' and '+query_map['ontology_instances']['value']+'=%s) or '
-                v+=(k,m)
-            q+=s[:-4]+') intersect '
-        for k,l in dp.iteritems():
-            s = 'select target_guid from ontology_terms a, ontology_instances c where c.term_guid=a.ot_guid and ('
-            for m in l:
-                s+= '('+query_map['ontology_terms']['uid']+'=getTermUidByPath(%s)'+' and '+query_map['ontology_instances']['value']+'=%s) or '
                 v+=(k,m)
             q+=s[:-4]+') intersect '
         q=q[:-11]+')'
